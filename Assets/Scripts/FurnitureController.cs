6792ee1ef5efa2f05091c755e6553fbd64afe278
VladVeen <veenine99@mail.ru>
2021-01-14 13:56:33 +0300

    Доделал приложение
    Добавил вращение объектам

---
+++ Assets/Scripts/FurnitureController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _chairArray;
+    [SerializeField] private GameObject[] _tableArray;
+    [SerializeField] private GameObject[] _sofaArray;
+    [SerializeField] private GameObject[] _armchairArray;
+    [SerializeField] private GameObject[] _nightstandArray;
+    
+    public static GameObject СurrentFurniture;
+    private GameObject[] _currentCategory;
+
+
+    public void SelectCategory(int selectedCategory)
+    {
+        switch (selectedCategory)
+        {
+            case 0:
+                _currentCategory = _chairArray;
+                break;
+            case 1:
+                _currentCategory = _tableArray;
+                break;
+            case 2:
+                _currentCategory = _sofaArray;
+                break;
+            case 3:
+                _currentCategory = _armchairArray;
+                break;
+            case 4:
+                _currentCategory = _nightstandArray;
+                break;
+        }
+    }
+
+    public void SelectModel(int modelNumber)
+    {
+        СurrentFurniture = _currentCategory[modelNumber];
+    }
+}
---
+++ Assets/Scripts/ShowObjectScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShowObjectScene : MonoBehaviour
+{
+    [SerializeField] private GameObject _groundPlaneStage;
+    private GameObject _currentObject;
+    private Quaternion _yRotation;
+
+    private void Update()
+    {
+        RotationObject();
+    }
+
+    public void PutObject()
+    {
+        GameObject newObject = Instantiate(FurnitureController.СurrentFurniture) as GameObject;
+        newObject.transform.SetParent(_groundPlaneStage.transform, false);
+
+        _currentObject = newObject;
+    }
+
+    private void RotationObject()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                _yRotation = Quaternion.Euler(0f, -touch.deltaPosition.x * 0.1f, 0f);
+                _currentObject.transform.rotation = _yRotation * _currentObject.transform.rotation;
+            }
+        }
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
+}
